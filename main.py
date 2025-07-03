7dc1b0985ec94f56d51317bca31cf8cb58c1c06a
zeffer <zbrown005@gmail.com>
2025-07-03 16:22:24 -0400

    update main.py for word count

--- main.py
+++ main.py
@@ -3,5 +3,17 @@ def get_book_text(file_path):
         file_contents = f.read()
         return file_contents
     
-def main():
-    return get_book_text("~/mnt/c/users/zbrow/workspace/github.com/bootdev/bookbot/books/frankenstein.txt")
+def print_book():
+    return get_book_text("books/frankenstein.txt")
+
+    
+def get_num_words():
+   num_words = 0
+   words = print_book().split()
+
+   for word in words:
+       num_words += 1
+   message = f"{num_words} words found in the document"
+   return message
+
+print(get_num_words())
