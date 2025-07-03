661d0f804664581ae045b1768dc37e3951586c3b
zeffer <zbrown005@gmail.com>
2025-07-03 14:59:09 -0400

    added main.py and .gitignore

---
+++ main.py
@@ -0,0 +1,7 @@
+def get_book_text(file_path):
+    with open(file_path) as f:
+        file_contents = f.read()
+        return file_contents
+    
+def main():
+    return get_book_text("~/mnt/c/users/zbrow/workspace/github.com/bootdev/bookbot/books/frankenstein.txt")
